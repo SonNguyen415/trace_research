5805db2ea7e4a0fc860a3ddf229696178633f00d
Son Nguyen <Son.mofa415@gmail.com>
2024-05-02 01:04:02 -0400

    Resolved Issues

--- main.c
+++ main.c
@@ -331,7 +331,7 @@ void * test8_thread_b(void * arg) {
     bool res = false;
 
     // Enqueue to the ring buffer twice
-    for(int i=0; i<2; i++) {
+    for(int i=0; i<3; i++) {
         res = ENQUEUE_TRACE(format, 6, args);
         assert(res);
     }
